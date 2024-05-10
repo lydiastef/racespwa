62c21cf2ea77fadc25cb72bf66d720db5c72d73a
Lydia <lydiadoula@gmail.com>
2024-05-10 20:48:53 +0000

    offline

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,31 @@ const withSerwist = withSerwistInit({
   swDest: "public/sw.js",
 });
 
+const withPWA = require("@ducanh2912/next-pwa").default({
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  dest: "public",
+  fallbacks: {
+    //image: "/static/images/fallback.png",
+    document: "/offline", // if you want to fallback to a custom page rather than /_offline
+    // font: '/static/font/fallback.woff2',
+    // audio: ...,
+    // video: ...,
+  },
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+  // ... other options you like
+});
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // ... other options you like
+};
+
+module.exports = withPWA(nextConfig);
+
 export default withSerwist({
   // Your Next.js config
 });
---
+++ src/app/offline/page.tsx
@@ -0,0 +1,21 @@
+//import styles from "../page.module.css";
+//import Image from "next/image"
+//import Link from "next/link";
+
+const NotFound: React.FC = () => {
+    return (
+        <>
+            <div>                
+                <h1>oh no!</h1>
+                <h1>There is no internet</h1>
+                <h2>It&apos;s a good idea to have an offline page because I might not be precaching 
+                    everything and you don&apos;t want your users to get stuck on a blank screen.
+                    But this page needs some more work though! I haven&apos;t even connected it yet. </h2>
+            </div> 
+        </>
+    )
+}
+
+export default NotFound 
+
+//Click here to go back to landing page
